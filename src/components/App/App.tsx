aad232863eb4b184ed227eff2370de5d21565b4b
Kamil Żygowski <kzygowski7@gmail.com>
2022-10-30 05:28:43 +0100

    init and basic config

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Dashboard from '../Dashboard/dashboard';
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Dashboard/dashboard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./dashboard.scss";
+import image from "../../assets/x.png"
+
+const Dashboard = () => {
+  return (
+    <div className="Dashboard">
+        Dashboard
+        <img src={image} alt="xd"/>
+    </div>
+  )
+}
+
+export default Dashboard
