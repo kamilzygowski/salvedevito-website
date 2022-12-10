020611bce207dfb20e66ae8a07174bacf45ceae3
Kamil Żygowski <kzygowski7@gmail.com>
2022-12-10 18:21:01 +0100

    Loading screen and .env

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,26 +1,23 @@
 import React, { useRef, useState } from 'react'
 import './Login.scss'
 import logo from "../../assets/logo.png"
-import {useHistory} from "react-router-dom"
+import { useHistory } from "react-router-dom"
 import { useTranslation } from "react-i18next"
+import { Rings } from 'react-loader-spinner'
+import * as dotenv from "dotenv"
 
 const Login = () => {
-    const history:any = useHistory()
+  const history = useHistory()
   const { t } = useTranslation()
   const label0: React.MutableRefObject<any> = useRef(null)
   const label1: React.MutableRefObject<any> = useRef(null)
-  const label2: React.MutableRefObject<any> = useRef(null)
-  const label3: React.MutableRefObject<any> = useRef(null)
-  const labels: React.MutableRefObject<any>[] = [label0, label1, label2, label3]
+  const labels: React.MutableRefObject<any>[] = [label0, label1]
   const [email, setEmail] = useState<string>("")
   const [password, setPassword] = useState<string>("")
-  const [confirmPassword, setConfirmPassword] = useState<string>("")
-  const [name, setName] = useState<string>("")
+  const [isLoading, setLoading] = useState<boolean>(false)
   const emailInput = useRef(null)
   const passwordInput = useRef(null)
-  const confirmPasswordInput = useRef(null)
-  const nameInput = useRef(null)
-  const input_values: any[] = [emailInput, passwordInput, confirmPasswordInput, nameInput]
+  const input_values: any[] = [emailInput, passwordInput]
   const onInputSelect = (e: any) => {
     try {
       labels.forEach((element: React.MutableRefObject<any>, index: number) => {
@@ -40,22 +37,40 @@ const Login = () => {
 
     }
   }
+  const handleLogin = () => {
+    console.log(email, password)
+    setLoading(true);
+    //history.push('/account')
+    console.log(process.env.REACT_APP_API_URL)
+  }
   window.addEventListener('click', (event: MouseEvent) => {
     onInputSelect(event)
   });
   return (
-    <div className='Register' id='-1' onClick={onInputSelect}>
-      <img src={logo} alt="" />
-      {/*<h1>{t("register-get-started")}</h1>*/}
-      <form autoComplete='off' onSubmit={() => history.pushState('/account')}>
-        <label ref={label0}><span>{t("register-email-input")}</span></label>
-        <input ref={emailInput} type="email" onSelect={onInputSelect} id="0" onChange={(val: any) => setEmail(val)} autoComplete="none" autoCorrect='off'/>
-        <label ref={label1}><span>{t("register-password-input")}</span></label>
-        <input ref={passwordInput} type="password" onSelect={onInputSelect} id="1" onChange={(val: any) => setPassword(val)} autoComplete="none" autoCorrect='off'/>
-        <button type="submit">
-          {t("login-button")}
-        </button>
-      </form>
+    <div className={isLoading ? 'LoginOff' : 'Login'} id='-1' onClick={onInputSelect}>
+            {/*<h1>{t("register-get-started")}</h1>*/}
+      {isLoading ? <Rings
+        height='200'
+        width='200'
+        radius='1'
+        color='#f1f2f6'
+        ariaLabel='loading' 
+        wrapperClass='loading'
+        wrapperStyle={{marginTop: '200px'}}
+        /> :  
+          
+          <form autoComplete='off' onSubmit={handleLogin}>
+            <img src={logo} alt="" />
+            <label ref={label0}><span>{t("register-email-input")}</span></label>
+            <input ref={emailInput} type="email" onSelect={onInputSelect} id="0" onChange={(val: any) => setEmail(val.target.value)} autoComplete="none" autoCorrect='off' />
+            <label ref={label1}><span>{t("register-password-input")}</span></label>
+            <input ref={passwordInput} type="password" onSelect={onInputSelect} id="1" onChange={(val: any) => setPassword(val.target.value)} autoComplete="none" autoCorrect='off' />
+            <button type="submit">
+              {t("login-button")}
+            </button>
+          </form>
+        }
+
     </div>
   )
 }
