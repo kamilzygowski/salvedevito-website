77ddaa2c1eb2749e60cf9fcb4197aaae901832e5
Kamil Żygowski <kzygowski7@gmail.com>
2022-12-13 00:49:06 +0100

    Downloads Section preparation

--- src/pages/Downloads/Downloads.tsx
+++ src/pages/Downloads/Downloads.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import { useTranslation } from 'react-i18next'
+import "./Downloads.scss"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCloudArrowDown } from '@fortawesome/free-solid-svg-icons'
 const Downloads = () => {
+  const { t } = useTranslation()
   return (
-    <div>Downloads</div>
+    <div className='Downloads'>
+      <div className='banner'>
+      <div className='download-button-wrapper'>
+        <p>{t("download-client")}</p>
+        <FontAwesomeIcon icon={faCloudArrowDown} className="icon"/>
+        </div>
+      </div>
+      
+        <div className='requirements'>
+
+        </div>
+    </div>
   )
 }
 
